171ee17ad5525ee70f14417e629f79cfdf0843fb
alejandro <alex93lzn@gmail.com>
2020-02-22 16:01:27 +0100

    ahora he hecho un cambio

--- src/pruebaGIT/ProbandoGIT.java
+++ src/pruebaGIT/ProbandoGIT.java
@@ -5,6 +5,7 @@ public class ProbandoGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellooooo");
+		System.out.println("Cambioo");
 	}
 
 }
